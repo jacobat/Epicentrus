90e22cecb74ca6d57d8dee808f7453d9333069c0
Jacob Atzen <jacob@incremental.dk>
2011-02-26 21:01:49 +0100

    Public caching

--- app/controllers/tweetstats_controller.rb
+++ app/controllers/tweetstats_controller.rb
@@ -1,6 +1,6 @@
 class TweetstatsController < ApplicationController
   def index
-    if(stale?(:etag => Tweet.latest, :last_modified => Tweet.latest.created_at.utc))
+    if(stale?(:etag => Tweet.latest, :last_modified => Tweet.latest.created_at.utc, :public => true))
       render :json => Tweetstat.new(2.days.ago.localtime..Time.now.localtime).run
     end
   end
