71114bb987fd26854f8064c9d7645e68a7727d92
Jacob Atzen <jacob@incremental.dk>
2011-02-26 21:05:12 +0100

    Varnish debugging

--- app/controllers/tweetstats_controller.rb
+++ app/controllers/tweetstats_controller.rb
@@ -1,5 +1,6 @@
 class TweetstatsController < ApplicationController
   def index
+    expires_in(0, :public => true)
     if(stale?(:etag => Tweet.latest, :last_modified => Tweet.latest.created_at.utc, :public => true))
       render :json => Tweetstat.new(2.days.ago.localtime..Time.now.localtime).run
     end
