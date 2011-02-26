059d78ede678aa96d93b2962be33f4ca07519758
Jacob Atzen <jacob@incremental.dk>
2011-02-26 21:08:00 +0100

    Don't do public caching

--- app/controllers/tweetstats_controller.rb
+++ app/controllers/tweetstats_controller.rb
@@ -1,7 +1,6 @@
 class TweetstatsController < ApplicationController
   def index
-    expires_in(0, :public => true)
-    if(stale?(:etag => Tweet.latest, :last_modified => Tweet.latest.created_at.utc, :public => true))
+    if(stale?(:etag => Tweet.latest, :last_modified => Tweet.latest.created_at.utc))
       render :json => Tweetstat.new(2.days.ago.localtime..Time.now.localtime).run
     end
   end
