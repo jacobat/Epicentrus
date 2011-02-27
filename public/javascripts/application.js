cf14080bd668f4be070b30389aec0f00943bec09
Jacob Atzen <jacob@incremental.dk>
2011-02-27 13:58:56 +0100

    Linkify URL's

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery.fn.autolink = function () {
+    return this.each( function(){
+        var re = 
+/((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g;
+        $(this).html( $(this).html().replace(re, '<a href="$1">$1</a> ') );
+    });
+}
